7a70470f3770970245c636c7ab9ed4fd94d91bc8
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-01-09 07:47:16 +0530

    Time: 143 ms (96.57%), Space: 110.7 MB (99.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,38 +11,207 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-       if (head == nullptr || head->next == nullptr) {
-            return true; // An empty list or a list with a single element is a palindrome
-        }
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = nullptr;
-
-        // Reverse the first half of the linked list while finding the middle
-        while (fast != nullptr && fast->next != nullptr) {
-            fast = fast->next->next;
-
-            ListNode* nextNode = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nextNode;
-        }
-
-        // If the linked list has an odd number of elements, skip the middle node
-        if (fast != nullptr) {
-            slow = slow->next;
-        }
-
-        // Compare the reversed first half with the second half
-        while (prev != nullptr && slow != nullptr) {
-            if (prev->val != slow->val) {
-                return false;
-            }
-            prev = prev->next;
-            slow = slow->next;
-        }
-
-        return true;
+      if(head == nullptr || head->next == nullptr)
+      {
+             return true;
+       }
+         
+         ListNode* slow=head;
+         ListNode* fast=head;
+         ListNode* prev=nullptr;
+         while(fast != nullptr && fast->next != nullptr)
+         {
+             fast=fast->next->next;
+             ListNode* nextnode=slow->next;
+             slow->next=prev;
+             prev=slow;
+             slow=nextnode;
+                 
+         }
+         if(fast != nullptr)
+         {
+            slow=slow->next;
+         }
+         
+         while(prev != nullptr && slow != nullptr)
+         {
+             if(prev->val != slow->val)
+             {
+                 return false;
+             }
+             prev=prev->next;
+             slow=slow->next;
+         }
+         return true;
+         
+         
     }
 };
+         
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         
+       

f92ba9d0fe21ade9d0cd5811acadd9bcb6e3a372
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-02-18 16:32:03 +0530

    Time: 3307 ms (12.35%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT sp.name
+FROM SalesPerson sp
+WHERE sp.sales_id NOT IN (
+    SELECT DISTINCT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);

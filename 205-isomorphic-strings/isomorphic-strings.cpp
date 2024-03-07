718cdb46224a7457d146b83dec184eff6b2e14f4
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-03-07 17:36:36 +0530

    Time: 4 ms (76.92%) | Memory: 9.3 MB (33.16%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        unordered_map<char,char> str1;
+        unordered_map<char,char> str2;
+
+        for(int i=0; i< s.length(); i++)
+        {
+
+            char s_char=s[i];
+            char t_char=t[i];
+
+            if(str1.find(s_char) == str1.end())
+            {
+                str1[s_char]= t_char;
+            }
+            else
+            {
+                if(str1[s_char] != t_char)
+                {
+                    return false;
+                }
+            }
+
+            //check for t to s
+
+            if(str2.find(t_char) == str2.end())
+            {
+                str2[t_char] = s_char;
+            }
+            else
+            {
+                if(str2[t_char] != s_char)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+};

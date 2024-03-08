57515c167fbda159c7c8f4798f8d26147022a05d
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-03-08 15:21:05 +0530

    Time: 31 ms (5.15%) | Memory: 27.7 MB (6.53%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map<int,int> check;
+        unordered_set<int> check(nums.begin(), nums.end());
         int s=nums.size();
-        sort(nums.begin(),nums.end());
-        int max=nums[s-1];
-        for(auto n: nums)
-        {
-            check[n]=1;
-        }
+       
+       
+       
         for(int i=0;i<=s;i++)
         {
             if(check.find(i) == check.end())

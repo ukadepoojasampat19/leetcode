d4fc57447f6bc5a153c719ce3ee4162e4dabba65
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-03-13 18:41:48 +0530

    Time: 145 ms (62.15%) | Memory: 38.2 MB (67.12%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+
+          unordered_set<string> dead(deadends.begin(),deadends.end());
+          unordered_set<string> visit;
+          queue<string> q;
+          q.push("0000");
+          visit.insert("0000");
+          int level=0;
+          while(!q.empty())
+          {
+            int sz=q.size();
+            for(int i=0;i<sz;i++)
+            {
+                string curr = q.front();
+                q.pop();
+
+                if(dead.count(curr)) continue;
+                if(curr == target)  return level;
+
+                for(int j=0;j<4;j++)
+                {
+                    string up=curr;
+                    string down=curr;
+
+                    up[j]= (up[j] == '9') ? '0': up[j]+1;
+                    down[j]=(down[j] == '0') ? '9': down[j] -1;
+
+                    if( !visit.count(up) && !dead.count(up))
+                    {
+                        q.push(up);
+                        visit.insert(up);
+                    
+                    }
+                    if(!visit.count(down) && !dead.count(down))
+                    {
+                         q.push(down);
+                         visit.insert(down);
+                    }
+                }
+
+
+            }
+            level++;
+          }
+
+
+        return -1;
+    }
+};

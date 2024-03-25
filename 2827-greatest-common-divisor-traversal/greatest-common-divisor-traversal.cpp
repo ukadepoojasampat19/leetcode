19e85b924cb8c9f6d5de491d164d1fb1d4342a4b
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-03-25 06:48:46 +0530

    Time: 1039 ms (35.70%) | Memory: 209.1 MB (22.75%) - LeetSync

--- 2827-greatest-common-divisor-traversal/greatest-common-divisor-traversal.cpp
+++ 2827-greatest-common-divisor-traversal/greatest-common-divisor-traversal.cpp
@@ -1,83 +1,49 @@
-
-class DSU {
-public:
-    vector<int> dsu;
-    vector<int> size;
-
-    DSU(int N) : dsu(N + 1), size(N + 1, 1) {
-        for (int i = 0; i <= N; i++) {
-            dsu[i] = i;
-        }
-    }
-
-    int find(int x) {
-        return dsu[x] == x ? x : (dsu[x] = find(dsu[x]));
-    }
-
-    void merge(int x, int y) {
-        int fx = find(x);
-        int fy = find(y);
-        if (fx == fy) {
-            return;
-        }
-        if (size[fx] > size[fy]) {
-            swap(fx, fy);
-        }
-        dsu[fx] = fy;
-        size[fy] += size[fx];
-    }
-};
 class Solution {
 public:
-    bool canTraverseAllPairs(vector<int>& nums) {
-        const int MAX = 100000;
-        const int N = nums.size();
-        vector<bool> has(MAX + 1, false);
-        for (int x : nums) {
-            has[x] = true;
-        }
+    unordered_map<int, vector<int>> prime2index;
+    unordered_map<int, vector<int>> index2prime;
+    void dfs(int index, vector<int>& visitedIndex, unordered_map<int,bool>& visitedPrime){
+        if(visitedIndex[index] == true) return;
+        visitedIndex[index] = true;
 
-        // Edge cases
-        if (N == 1) {
-            return true;
-        }
-        if (has[1]) {
-            return false;
-        }
-
-        // General solution
-        vector<int> sieve(MAX + 1, 0);
-        for (int d = 2; d <= MAX; d++) {
-            if (sieve[d] == 0) {
-                for (int v = d; v <= MAX; v += d) {
-                    sieve[v] = d;
-                }
+        for(auto &prime : index2prime[index]){
+            if(visitedPrime[prime] == true) 
+                continue;
+            visitedPrime[prime] = true;
+            for(auto &index1 : prime2index[prime]){
+                if(visitedIndex[index1] == true) continue;
+                dfs(index1, visitedIndex, visitedPrime);
             }
         }
-
-        DSU unionFind(2 * MAX + 1);
-        for (int x : nums) {
-            int val = x;
-            while (val > 1) {
-                int prime = sieve[val];
-                int root = prime + MAX;
-                if (unionFind.find(root) != unionFind.find(x)) {
-                    unionFind.merge(root, x);
-                }
-                while (val % prime == 0) {
-                    val /= prime;
-                }
-            }
-        }
-
-        int cnt = 0;
-        for (int i = 2; i <= MAX; i++) {
-            if (has[i] && unionFind.find(i) == i) {
-                cnt++;
-            }
-        }
-        return cnt == 1;
     }
-     
-    
+
+    bool canTraverseAllPairs(vector<int>& nums) {
+        for (int i=0; i<nums.size(); i++) {
+            int temp = nums[i];
+            for (int j = 2; j*j <= nums[i]; j++) {
+                if (temp % j == 0) {
+                    prime2index[j].push_back(i);
+                    index2prime[i].push_back(j);
+                    while (temp % j == 0)
+                      temp /= j;
+                }
+            }
+            if (temp > 1) {
+                prime2index[temp].push_back(i);
+                index2prime[i].push_back(temp);
+            }
+        }
+
+        vector<int> visitedIndex(nums.size(),0);
+        unordered_map<int,bool> visitedPrime;
+        dfs(0, visitedIndex, visitedPrime);     
+
+        for(int i=0; i<visitedIndex.size(); i++) 
+            if(visitedIndex[i] == false) 
+                return false;
+        return true;    
+    }
 };
+
+
+

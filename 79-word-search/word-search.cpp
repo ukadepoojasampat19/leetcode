861ca84b889a4eceb2b6001d336891916875702d
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-04-03 22:41:13 +0530

    Time: 188 ms (93.91%) | Memory: 9.5 MB (96.96%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,37 @@
+
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
+        rows = board.size();
+        cols = board[0].size();
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (dfs(board, word, i, j, 0))
                     return true;
-                }
             }
         }
         return false;
+    }
+    
+private:
+    int rows, cols;
+    
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k) {
+        if (i < 0 || j < 0 || i >= rows || j >= cols || board[i][j] != word[k])
+            return false;
+        if (k == word.length() - 1)
+            return true;
+        
+        char temp = board[i][j];
+        board[i][j] = '*'; // Mark the cell as visited
+        
+        bool found = dfs(board, word, i + 1, j, k + 1) ||
+                     dfs(board, word, i - 1, j, k + 1) ||
+                     dfs(board, word, i, j + 1, k + 1) ||
+                     dfs(board, word, i, j - 1, k + 1);
+        
+        board[i][j] = temp; // Restore the original value
+        
+        return found;
     }
 };

6b14359aa34ec287a1c0e05119d1f9b6e00c632d
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-04-09 09:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (99.09%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+         
+       /*int sum=0;
+        int num=tickets[k];
+        for(auto c: tickets)
+        {
+            if(c <= num)
+            {
+                sum+=c;
+            }
+            else if(c > num) 
+            {
+                sum+=num;
+            }
+        }
+        return sum;*/
+         int n = tickets.size();
+        int time = 0;
+        
+        while (tickets[k] > 0) {
+            for (int i = 0; i < n; ++i) {
+                if (tickets[i] > 0) {
+                    tickets[i]--;
+                    time++;
+                    if (i == k && tickets[k] == 0)
+                        return time;
+                }
+            }
+        }
+        
+        return time;
+        
+    }
+};

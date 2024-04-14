17d38156da565846f1e343d822a90470150c77d9
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-04-14 23:15:26 +0530

    Time: 1038 ms (70.82%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -4,9 +4,12 @@ from Employees e
 left join Salaries s on e.employee_id = s.employee_id
 where e.name is null or s.salary is null
 order by employee_id asc;*/
-select employee_id from employees where employee_id NOT IN
-(select employee_id from salaries)
-UNION
-select employee_id from salaries where employee_id NOT IN
-(select employee_id from Employees )
-order by employee_id
+
+
+
+select employee_id  from Employees where employee_id not in 
+(select employee_id from Salaries)
+union
+select employee_id from Salaries where employee_id not in
+(select employee_id from Employees)
+order by employee_id asc;

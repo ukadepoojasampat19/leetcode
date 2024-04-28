ae6885e7570b373939ef8ed4c3a7dd627b796fc6
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-04-28 12:09:32 +0530

    Time: 71 ms (35.36%) | Memory: 87.8 MB (11.71%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,18 +1,69 @@
 class Solution {
+    int n_r,n_k;
+    int dp[101][101];
 public:
-    int findRotateSteps(string ring, string key) {
-  unordered_map<char, vector<int>> positions;
+    int solve(string ring,string key,int i,int j)
+    {
+        //base condition
+        if(i>=n_r || j>=n_k) return 0;
+
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        //key == ring then find next key 
+        if(ring[i] == key[j])  return dp[i][j]= 1+ solve(ring,key,i,j+1);
+
+        //ckeck clockwise and anticlockwise
+        int lefti=i,righti=i;
+        int dist1=0,dist2=0;
+
+        //first left
+        while(ring[lefti] != key[j])
+        {
+            lefti--;
+            if(lefti <0) lefti=n_r-1;
+            dist1++;
+        }
+
+        //right
+        while(ring[righti] !=key[j])
+        {
+            righti++;
+            if(righti == n_r) righti=0;
+            dist2++;
+
+        }
+        //if both are at same locaton
+        if(lefti==righti)
+        {
+            return dp[i][j]=min(dist1,dist2) +1 +solve(ring,key,lefti,j+1);
+        }
+        int ansL=dist1+1+solve(ring,key,lefti,j+1);
+        int ansR=dist2+1+solve(ring,key,righti,j+1);
+
+        return dp[i][j]= min(ansL,ansR);
+    }
+   int findRotateSteps(string ring, string key) {
+      /*  unordered_map<char, vector<int>> positions;
         for (int i = 0; i < ring.size(); ++i) {
             positions[ring[i]].push_back(i);
         }
         vector<vector<int>> memo(key.size(), vector<int>(ring.size(), -1));
-        return helper(0, 0, positions, key, ring, memo);
+        return helper(0, 0, positions, key, ring, memo);*/
+        /*----------------------------------------------------------------------------------------------------*/
+        n_r=ring.size();
+        n_k=key.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(ring,key,0,0);
+
+
     }
-    
-    int helper(int in_index, int pos, unordered_map<char, vector<int>>& positions, string& key, string& ring, vector<vector<int>>& memo) {
+};
+    /*int helper(int in_index, int pos, unordered_map<char, vector<int>>& positions, string& key, string& ring, vector<vector<int>>& memo) {
         if (in_index == key.size()) {
             return 0;
-        }
+     }
         if (memo[in_index][pos] != -1) {
             return memo[in_index][pos];
         }
@@ -29,5 +80,5 @@ public:
         }
         memo[in_index][pos] = min_steps + 1;
         return min_steps + 1;
-    }
-};
+        */
+

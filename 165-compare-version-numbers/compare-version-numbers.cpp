2b02f8562e2194bfb98347e40e554968b430dedd
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-05-03 06:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (77.28%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i=0,j=0;
+        int v1len=version1.length();
+        int v2len=version2.length();
+        int n1,n2;
+        while(i < v1len || j< v2len)
+        {
+            n1=0;
+            n2=0;
+            while(i<v1len && version1[i] !='.')
+            {
+                n1=n1*10 +(version1[i] - '0');
+                i++;
+            }
+            while(j <v2len && version2[j] !='.')
+            {
+                n2=n2*10 +(version2[j] - '0');
+                j++;
+            }
+
+            if(n1 < n2) return -1;
+            if(n1 > n2) return 1;
+            i++;
+            j++;
+        }
+       return 0;
+        
+    }
+};

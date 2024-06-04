8002a640be68bc0ae3f45c4e80591f64160ca8b9
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-04 10:18:43 +0530

    Time: 3 ms (47.00%) | Memory: 7.6 MB (31.12%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        bool check=false;
+        for(int i =0;i<t.length();i++)
+        {
+            if(s[j] == t[i])
+            {
+                j++;
+                
+
+            }
+        }
+        if(j == s.length())
+        {
+            return true;
+        }
+        return false;
+    }
+};

aff7a658ea634c942de92abddee7a4786e139d1f
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-07 18:01:19 +0530

    Time: 16 ms (53.98%) | Memory: 21.5 MB (95.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
-     int zero =0;
+    /* int zero =0;
      for(int i=0;i< nums.size();i++)
      {
         if(nums[i] != 0)
@@ -14,7 +14,16 @@ public:
      {
         nums[i] =0;
      }
-
+*/
+    int left=0;
+    for(int right=0;right< nums.size(); right++)
+    {
+        if(nums[right] != 0)
+        {
+            swap(nums[left],nums[right]);
+            left++;
+        }
+    }
         
     }
 };

027d195f489d95107e1ed7227bd2f8e8813ce63e
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-12 19:46:11 +0530

    Time: 2 ms (44.06%) | Memory: 9.9 MB (5.81%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        map<int, int> mp;
+       /* map<int, int> mp;
         for(auto &n: nums)
         {
             mp[n]++;
@@ -14,6 +14,31 @@ public:
             {
                 nums[i] = n.first;
                 i++;
+            }
+        }*/
+
+        int n = nums.size();
+        int i=0; // denote 0
+        int j=0; //denote 1
+        int k=n-1; // denote 2
+
+        while(j <= k)
+        {
+            if(nums[j] == 0)
+            {
+                swap(nums[j],nums[i]);
+                j++;
+                i++;
+
+            }
+            else if(nums[j] == 1)
+            {
+                j++;
+            }
+            else
+            {
+                swap(nums[j],nums[k]);
+                k--;
             }
         }
         

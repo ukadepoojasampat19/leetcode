6008ef8c55739daecfc1e7e4a7cf777184b962f7
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-13 18:49:49 +0530

    Time: 6 ms (62.74%) | Memory: 20.9 MB (52.06%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+
+        int sum=0;
+        for(int i=0;i<students.size();i++)
+        {
+            sum += abs(students[i] - seats[i]);
+        }
+        return sum;
+    }
+};

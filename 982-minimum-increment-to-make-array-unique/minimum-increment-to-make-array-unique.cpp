cdeb555d5f77f8beaf4cf747c51cef2d63bb459d
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-14 11:07:21 +0530

    Time: 122 ms (52.95%) | Memory: 69.1 MB (50.39%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,33 +1,21 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int minIncrementForUnique(std::vector<int>& nums) {
-        if (nums.empty()) return 0;
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
 
-        // Find the maximum value in nums to determine the size of the frequency array
-        int maxValue = *max_element(nums.begin(), nums.end());
+        int moves =0;
 
-        // Create a frequency array to count the occurrences of each number
-        std::vector<int> freq(maxValue + nums.size(), 0);
-        for (int num : nums) {
-            freq[num]++;
-        }
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i] <= nums[i-1])
+            {
+                int incr = nums[i-1] - nums[i] + 1;
+                nums[i] += incr;
 
-        int moves = 0;
-        int taken = 0;
-
-        for (int i = 0; i < freq.size(); ++i) {
-            if (freq[i] > 1) {
-                taken += freq[i] - 1;
-                moves -= (freq[i] - 1) * i;
-            } else if (freq[i] == 0 && taken > 0) {
-                taken--;
-                moves += i;
+                moves += incr;
+           
             }
         }
-
         return moves;
     }
 };

11df76e2afc39ad5c1a47e78cd8b28eb47f222b0
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-06-23 13:13:04 +0530

    Time: 138 ms (33.92%) | Memory: 80.9 MB (30.80%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        typedef pair<int,int> P;
+       /* typedef pair<int,int> P;
        int l=0; 
        int r=0;
        int n= nums.size();
        int max_value =0;
       priority_queue<P,vector<P>> max_q;
       priority_queue<P,vector<P>,greater<P>> min_q;
-       
+       //space 0(n+n);
       // sort(nums.begin(),nums.end());
        while(r<n)
        {
@@ -17,9 +17,9 @@ public:
            while(max_q.top().first - min_q.top().first > limit)
            {
                l = min(max_q.top().second,min_q.top().second) + 1;
-                while(max_q.top().second < l)
+                while(max_q.top().second < l) // top sathi constant time 
                 {
-                    max_q.pop();
+                    max_q.pop(); // log(n)
                 }
                 while(min_q.top().second < l)
                 {
@@ -31,6 +31,21 @@ public:
 
        }
         return max_value;
-       
+       // o(nlog(n));*/
+       multiset<int> st;
+       int n= nums.size();
+       int i=0,j=0,mx_len=0;
+       while(j<n)
+       {
+            st.insert(nums[j]);
+            while(*st.rbegin() - *st.begin() > limit)
+            {
+                st.erase(st.find(nums[i]));
+                i++;
+            }
+            mx_len = max(mx_len,j-i+1);
+            j++;
+       }
+       return mx_len;
     }
 };

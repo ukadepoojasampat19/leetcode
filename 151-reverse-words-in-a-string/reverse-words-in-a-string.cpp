19202cd7fc75600b9d1d213cd9d772603100d690
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-18 12:14:32 +0530

    Time: 3 ms (81.59%) | Memory: 23.5 MB (10.54%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,42 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        //buid in function
-        string word;
-        string words="";
         istringstream iss(s);
+        string word="";
+        string words="";
         while(iss >> word)
         {
-            words=word+" "+words;
+            words =word + " " +words;
         }
+        return words.substr(0, words.length()-1);
         
-        return words.substr(0,words.size()-1);
-
-        //two pointer approach
-        /*reverse(s.begin(),s.end());
-        int l=0;
-        int r=0;
-        string result="";
-        int i=0;
-        int n=s.length();
-        while(i < n)
-        {
-            while(i < n && s[i] !=' ')
-            {
-                s[r++]=s[i++];
-
-            }
-            if(l <r)
-            {
-                 reverse(s.begin()+l, s.begin()+r);
-                 s[r]=' ';
-                    r++;
-                    l=r;
-            }
-           
-            i++;
-        }
-        return s.substr(0,r-1);*/
-
     }
 };

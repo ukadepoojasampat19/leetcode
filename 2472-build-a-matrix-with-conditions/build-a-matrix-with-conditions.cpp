34cdb2fde5568e24bcb7dc10de9511d2b7798fcf
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-21 20:20:39 +0530

    Time: 99 ms (64.51%) | Memory: 61.5 MB (73.24%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -1,66 +1,73 @@
 class Solution {
 public:
-    void dfs(int u, vector<int>& visited, unordered_map<int, vector<int>>& adj, stack<int>& st, bool& cycle) {
-        visited[u] = 1;
-        for (int &v : adj[u]) {
-            if (visited[v] == 0) {
-                dfs(v, visited, adj, st, cycle);
-            } else if (visited[v] == 1) {
-                cycle = true;
-                return;
-            }
-        }
-        visited[u] = 2;
-        st.push(u);
-    }
 
-    vector<int> toposort(vector<vector<int>>& edges, int n) {
+      vector<int> topoSort(vector<vector<int>>& edges, int n) {
         unordered_map<int, vector<int>> adj;
+        stack<int> st;
+        vector<int> order;
+        //0 : not visited;
+        //1 : visiting (currently in stack via some other DFS)
+        //2 : visited
+        vector<int> visited(n + 1, 0);
+        bool hasCycle = false;
+
         for (vector<int>& edge : edges) {
             int u = edge[0];
             int v = edge[1];
             adj[u].push_back(v);
         }
 
-        vector<int> visited(n + 1, 0);
-        vector<int> order;
-        stack<int> st;
-        bool cycle = false;
-
         for (int i = 1; i <= n; i++) {
             if (visited[i] == 0) {
-                dfs(i, visited, adj, st, cycle);
-                if (cycle == true) {
-                    return {};
-                }
+                dfs(i, adj, visited, st, hasCycle);
+                if (hasCycle) 
+                    return {}; //no ordering possible
             }
         }
-
-        while (!st.empty()) {
+        
+        while(!st.empty()) {
             order.push_back(st.top());
             st.pop();
         }
         return order;
     }
 
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        vector<int> row_value = toposort(rowConditions, k);
-        vector<int> col_value = toposort(colConditions, k);
+    void dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>& visited,
+             stack<int>& st, bool& hasCycle) {
+        
+        visited[node] = 1;  // Mark node as visiting
+        //First, visit node's children
+        for (int& nbr : adj[node]) {
+            if (visited[nbr] == 0) {
+                dfs(nbr, adj, visited, st, hasCycle);
+            } else if (visited[nbr] == 1) {
+                // Cycle detected
+                hasCycle = true;
+                return;
+            }
+        }
 
-        if (row_value.empty() || col_value.empty()) {
+        visited[node] = 2; //visited
+        st.push(node); //Now node can be added
+    }
+
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
+                                    vector<vector<int>>& colConditions) {        
+        vector<int> orderRows    = topoSort(rowConditions, k);
+        vector<int> orderColumns = topoSort(colConditions, k);
+
+        // We might have found cycle. That's why topo order is empty
+        if (orderRows.empty() or orderColumns.empty())
             return {};
-        }
 
-        vector<vector<int>> res(k, vector<int>(k, 0));
-        unordered_map<int, int> pos;
+        vector<vector<int>> matrix(k, vector<int>(k, 0));
         for (int i = 0; i < k; i++) {
-            pos[col_value[i]] = i;
+            for (int j = 0; j < k; j++) {
+                if (orderRows[i] == orderColumns[j]) {
+                    matrix[i][j] = orderRows[i];
+                }
+            }
         }
-
-        for (int i = 0; i < k; i++) {
-            res[i][pos[row_value[i]]] = row_value[i];
-        }
-
-        return res;
+        return matrix;
     }
 };

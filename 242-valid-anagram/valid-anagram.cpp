6f27bb91bb15953bad6e758fb531eaa1aa584af3
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-26 20:04:14 +0530

    Time: 7 ms (75.44%) | Memory: 10 MB (8.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length())
-        {
-            return false;
-        }
-        unordered_map<char,int> first;
-        unordered_map<char,int> second;
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int> str1;
+        unordered_map<char,int> str2;
         for(int i=0;i<s.length();i++)
         {
-            first[s[i]]++;
-            second[t[i]]++;
+            str1[s[i]]++;
         }
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<t.length();i++)
         {
-            if(second.find(s[i]) != second.end())
+            str2[t[i]]++;
+        }
+
+       for(auto& n: str1)
+       {
+            char ch = n.first;
+            int freq = n.second;
+            if(str2.find(ch) != str2.end())
             {
-                if(first[s[i]] == second[s[i]])
-                {
-                    continue;
-                }
-                else
-                {
-                    return false;
-                }
+                if(str2[ch] != freq) return false;
             }
             else
             {
                 return false;
             }
-        }
-        return true;
+       }
+       return true;
     }
 };

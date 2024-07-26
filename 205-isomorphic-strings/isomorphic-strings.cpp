a7f367846c327ed708899f127408e267964b36a3
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-26 18:57:50 +0530

    Time: 4 ms (76.00%) | Memory: 9.6 MB (12.21%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,68 +1,36 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       if(s.length() != t.length())
-        {
-            return false;
-        }
-        /*unordered_map<char,char> str1;
-        unordered_map<char,char> str2;
+        unordered_map<char , char> str1;
+        unordered_map<char , char> str2;
 
-        for(int i=0; i< s.length(); i++)
-        {
-            char s_char=s[i];
-            char t_char=t[i];
-
-            if(str1.find(s_char) == str1.end())
-            {
-                str1[s_char]= t_char;
-            }
-            else
-            {
-                if(str1[s_char] != t_char)
-                {
-                    return false;
-                }
-            }
-
-            //check for t to s
-
-            if(str2.find(t_char) == str2.end())
-            {
-                str2[t_char] = s_char;
-            }
-            else
-            {
-                if(str2[t_char] != s_char)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;*/
-        unordered_map<char,char> map_char;
-        unordered_set<char> set_vals;
+        if(s.length() != t.length()) return false;
 
         for(int i=0;i<s.length();i++)
         {
-            if(map_char.find(s[i]) != map_char.end())
+            char num1 = s[i];
+            char num2 = t[i];
+            
+            //check for s to t
+            if(str1.find(s[i]) != str1.end())
             {
-                 if(map_char[s[i]] != t[i])
-                 {
-                    return false;
-                 }
+                if(str1[num1] != num2) return false;
             }
             else
             {
-                if(set_vals.find(t[i]) != set_vals.end())
-                {
-                    return false;
-                }
-                map_char[s[i]]=t[i];
-                set_vals.insert(t[i]);
+                str1[num1] =  num2;    
+            }
+
+            //check for t to s
+            if(str2.find(t[i]) != str2.end())
+            {
+                if(str2[num2] != num1) return false;
+            }
+            else
+            {
+                str2[num2] =  num1;    
             }
         }
-
         return true;
         
     }

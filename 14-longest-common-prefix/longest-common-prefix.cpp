eb5538dc63d0b9d6685c2df257eb06221f47f9b5
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-26 18:19:53 +0530

    Time: 7 ms (28.19%) | Memory: 11.2 MB (32.13%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty())
-            {
-                return "";
-            }
-        string result="";
-        
-        for(int i=0;i<strs[0].length();i++)
-        {
-            char curr_char=strs[0][i];
-            
-            for(int j=1;j< strs.size();j++)
-            {
-                if(i >= strs[j].size() ||curr_char != strs[j][i])
-                {
-                   return result;
-                }
-                
-            }
-             result=result + curr_char;
-            
-               
-            
-        }
-
-        return result;
-        
+        if(strs.empty()) return "";
+       string result ="";
+       for(int i=0;i<strs[0].length();i++)
+       {
+         char currchar = strs[0][i];
+         for(int j=1;j< strs.size();j++)
+         {
+             if( strs[j][i] != currchar)
+             {
+                return result;
+             }
+         }
+         result += currchar;
+       }
+       return result;
     }
 };

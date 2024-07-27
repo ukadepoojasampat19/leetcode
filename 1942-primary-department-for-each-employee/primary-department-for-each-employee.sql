d2eda48a2f48043f6dd0de8841379aa962b7ed75
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-27 12:13:04 +0530

    Time: 566 ms (36.92%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,12 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y' OR employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(primary_flag) = 1
-)
+# Write your MySQL query statement below
+/*select employee_id, (case when (count(distinct department_id)) = 1 then department_id else primary_flag ='Y' end) as department_id
+from Employee
+group by employee_id*/
+select employee_id, department_id
+from Employee
+where primary_flag ='Y' or
+employee_id in
+(select employee_id 
+from Employee
+group by employee_id
+having count(employee_id) =1);

7c0b9e99848236df56a20411f6ec75373c1a8500
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-08-05 16:58:42 +0530

    Time: 529 ms (5.01%) | Memory: 16.2 MB (69.82%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int curr_sum = 0;
-        
-        // Calculate the total sum
-        for (int num : nums) {
-            curr_sum += num;
+        int curr_sum =0;
+        for(auto& n: nums)
+        {
+            curr_sum += n;
         }
-        
-        // If the total sum is odd, it can't be partitioned into two equal subsets
-        if (curr_sum % 2 != 0) {
-            return false;
-        }
-        
+
+        if(curr_sum % 2 != 0) return false;
         int target = curr_sum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true; // Base case: A sum of 0 is always achievable
-        
-        for (int num : nums) {
-            // Traverse dp array from right to left
-            for (int j = target; j >= num; --j) {
-                dp[j] = dp[j] || dp[j - num];
+        vector<vector<bool>> dp(nums.size()+1,vector<bool>(target+1,0));
+
+        dp[0][0] = true;
+
+        for(int i=1;i<=nums.size();i++)
+        {
+            for(int j=0;j<=target;j++)
+            {
+                    //not include
+                    dp[i][j] = dp[i-1][j];
+
+                    //include;
+                    if(nums[i-1]  <= j)
+                    {
+                        dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]];
+                    }
             }
         }
-        
-        return dp[target];
+        return dp[nums.size()][target];
     }
 };

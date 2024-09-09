2cffa6d792c13a96ad7538f78ac085c0e5ddd484
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-09-09 13:37:59 +0530

    Time: 4 ms (73.67%) | Memory: 13.7 MB (83.30%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -11,53 +11,38 @@
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        vector<ListNode*> ans(k);
+        int link_size = 0;
+        vector<ListNode*> result(k,nullptr);
+        ListNode* temp = head;
 
-        // get total size of linked list
-        int size = 0;
-        ListNode* current = head;
-        while (current != nullptr) {
-            size++;
-            current = current->next;
+        while(temp != nullptr)
+        {
+            link_size++;
+            temp=temp->next;
+        }
+        temp = head;
+        int partSize = link_size / k;
+        int rem = link_size % k;
+
+        for(int i=0;i<k;i++)
+        {
+            result[i] = temp;
+            int current_ct = partSize + (i < rem ? 1:0);
+            ListNode* prev = nullptr;
+
+            for(int j=0;j<current_ct;j++)
+            {
+                prev = temp;
+                temp= temp->next;
+            }
+            if(prev)
+            {
+                prev->next=nullptr;
+            }
         }
 
-        // minimum size for the k parts
-        int splitSize = size / k;
 
-        // Remaining nodes after splitting the k parts evenly.
-        // These will be distributed to the first (size % k) nodes
-        int numRemainingParts = size % k;
 
-        current = head;
-        ListNode* prev = current;
-        for (int i = 0; i < k; i++) {
-            // create the i-th part
-            ListNode* newPart = current;
-            // calculate size of i-th part
-            int currentSize = splitSize;
-            if (numRemainingParts > 0) {
-                numRemainingParts--;
-                currentSize++;
-            }
-
-            // traverse to end of new part
-            int j = 0;
-            while (j < currentSize) {
-                prev = current;
-                if (current != nullptr) {
-                    current = current->next;
-                }
-                j++;
-            }
-
-            // cut off the rest of linked list
-            if (prev != nullptr) {
-                prev->next = nullptr;
-            }
-
-            ans[i] = newPart;
-        }
-
-        return ans;
+        return result;
     }
 };

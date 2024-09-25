abc86b0df8ed3f22f397d4640bed5252ecab5bb0
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-09-25 17:53:13 +0530

    Time: 614 ms (43.14%) | Memory: 719.2 MB (35.91%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,82 @@
+struct Node
+{
+    Node* link[26];
+    int ct =0;
+
+    bool containkey(char ch)
+    {
+        return (link[ch -'a'] != NULL);
+    }
+    void put(char ch,Node* node)
+    {
+        link[ch - 'a'] = node;
+    }
+    Node* get(char ch)
+    {
+        return link[ch -'a'];
+    }
+    void increment()
+    {
+        ct += 1;
+    }
+    int getCount()
+    {
+        return ct;
+    }
+};
+
+
+
+class Solution {
+private:
+   Node* root;
+public:
+    Solution()
+    {
+        root = new Node();
+    }
+
+    //insert
+    void insert(string word)
+    {
+        Node* node = root;
+        for(int i=0;i<word.length();i++)
+        {
+            node->increment();
+            if(!node->containkey(word[i]))
+            {
+                node->put(word[i],new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->increment();
+    }
+
+    bool startWith(string prefix,vector<int>& res,int n)
+    {
+        Node* node = root;
+        int count =0;
+        for(int i=0;i<prefix.length();i++)
+        {
+            node= node->get(prefix[i]);
+            count += node->getCount();
+        }
+        res[n] = count;
+        return true;
+    }
+    vector<int> sumPrefixScores(vector<string>& words) {
+
+        Solution obj ;
+        int m = words.size();
+        vector<int> res(m);
+        for(int i=0;i<m;i++)
+        {
+            obj.insert(words[i]);
+        }
+        for(int i=0;i<words.size();i++)
+        {
+            obj.startWith(words[i],res,i);
+        }
+        return res;
+    }
+};

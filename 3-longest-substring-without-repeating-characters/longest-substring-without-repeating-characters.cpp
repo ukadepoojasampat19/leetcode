fb34b4ed838b9ed0ed2d1e42c97d8ac88f8ed8c8
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-06-14 11:22:12 +0530

    Time: 15 ms (37.61%) | Memory: 14.4 MB (12.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mp; // Map to track frequency of characters
-        int l = 0, r = 0;
-        int ct = 0; // Initialize to 0 to handle cases when the string is empty
-        
-        while (r < s.length()) {
-            mp[s[r]]++; // Increment the frequency of the character at `r`
-            
-            // If the character repeats (i.e., its count is greater than 1)
-            while (mp[s[r]] > 1) {
-                mp[s[l]]--; // Decrease the frequency of the character at `l`
-                l++; // Move the left pointer to shrink the window
+        unordered_set<char> st;
+        int n = s.length();
+        int l=0;
+        int r=0;
+        int maxlen = 0;
+
+        while(r < n){
+
+            while(st.find(s[r]) != st.end()){
+                st.erase(s[l]);
+                l++;   
             }
-            
-            // Update the length of the longest substring without repeating characters
-            ct = max(ct, r - l + 1);
-            
-            r++; // Expand the window by moving the right pointer
+            st.insert(s[r]);
+            maxlen = max(maxlen, r-l+1);
+            r++;
         }
-        
-        return ct;
+        return maxlen;
     }
 };

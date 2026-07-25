fd0b144e414f74887c186d7160e638bd69ffc8d3
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-25 14:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (75.42%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
+     int idx = -1;
 
-        // Step 1: Find the pivot
-        int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
+     int n = nums.size();
+     for(int i= n-2;i>=0;i--){
+        if(nums[i] < nums[i+1]){
+            idx = i;
+            break;
         }
-
-        // Step 2: If pivot exists, find the next greater element
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums[i], nums[j]);
+     }
+     if(idx == -1){
+        reverse(nums.begin(), nums.end());
+        return;
+     }
+     for(int i =n-1;i>idx;i--){
+        if(nums[i] > nums[idx]){
+            swap(nums[i], nums[idx]);
+            break;
         }
-
-        // Step 3: Reverse the suffix
-        reverse(nums.begin() + i + 1, nums.end());
+     }
+     reverse(nums.begin() + idx +1, nums.end());
+     
     }
 };

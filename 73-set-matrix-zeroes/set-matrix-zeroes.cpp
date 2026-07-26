582e6246f5102b66088584fffbdd4dba9a7ba644
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-26 13:47:02 +0530

    Time: 3 ms (28.80%) | Memory: 20.9 MB (51.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
 
-    void set_temp_zero(int i,int j, vector<vector<int>>& temp){
-        int m = temp[0].size();
-        int n = temp.size();
-            int row = i;
-            int col = j;
-        while(i>=0){
-             
-            temp[i][j] = 0;
-            i--;
-        }
-        i = row;
-         while(i<n){
-             
-            temp[i][j] = 0;
-            i++;
-        }
-        i = row;
-         while(j>=0){
-             
-            temp[i][j] = 0;
-            j--;
-        }
-        j = col;
-         while(j<m){
-             
-            temp[i][j] = 0;
-            j++;
-        }
-    }
+  
     void setZeroes(vector<vector<int>>& matrix) {
        
         int col = matrix[0].size();
         int row = matrix.size();
-        vector<vector<int>> temp =  matrix;
+
+        vector<int> r(row,0);
+        vector<int> c(col,0);
 
         for(int i=0; i<row; i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j] == 0){
-                    set_temp_zero(i,j, temp);
+                if(r[i] != 1){
+                    r[i] = 1;
+                }
+                if(c[j] != 1){
+                    c[j] = 1;
+                }
                 }
             }
         }
-        matrix = temp;
+
+        for(int i=0; i<row; i++){
+            for(int j=0;j<col;j++){
+               if(r[i] ==1 || c[j] == 1){
+                matrix[i][j] =0;
+               }
+            }
+        }
+        
+       
 
     }
 };

fff864c851edf2f4b5a46b7300beeda910035c89
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-26 19:30:09 +0530

    Time: 49 ms (45.61%) | Memory: 45.5 MB (56.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n= nums.size();
+
         unordered_map<int, int> mp;
-        int sum=0;
-        mp.insert({0,1});
-        int result=0;
 
-        for(int i=0;i< n;i++)
-        {
-            sum += nums[i];
-            int curr_val = sum - k;
+        // Prefix sum 0 has occurred once
+        mp[0] = 1;
 
-            if(mp.find(curr_val) != mp.end())
-            {
-                result += mp[curr_val];
-                //mp[curr_val]++;
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            // Check if there exists a previous prefix sum
+            // such that currentSum - previousSum = k
+            if (mp.find(prefixSum - k) != mp.end()) {
+                count += mp[prefixSum - k];
             }
-          
-            mp[sum]++;
-            
+
+            // Store current prefix sum
+            mp[prefixSum]++;
         }
 
-        return result;
-        
+        return count;
     }
 };

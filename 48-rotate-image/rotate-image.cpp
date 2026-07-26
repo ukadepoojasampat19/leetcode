4458c5a7385cfc81c8a7821d489f34f20251707c
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-26 16:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (93.96%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
   
-
-    
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+     //   int n = matrix.size();
        
-        vector<vector<int>> temp(n, vector<int>(n));
-        for(int i=0;i<n;i++){
+      //  vector<vector<int>> temp(n, vector<int>(n));
+       // for(int i=0;i<n;i++){
 
-            vector<int> res(n);
+           // vector<int> res(n);
 
-            for(int j=0;j<n;j++){
+           // for(int j=0;j<n;j++){
 
-                temp[j][n-1-i] =matrix[i][j];
-            }
+              //  temp[j][n-1-i] = matrix[i][j];
+         //   }
             
+       // }
+       //optimal approach o(n2) time complexity 
+        //space completit o(n*n); -> for storing the temp 2d vectore
+        //matrix = temp;
+
+        //optimal approach o(n2) time complexity 
+        //space completit o(1);
+
+        //calculate the transpose of the matrx(row become colum and column became row )
+        //avoid diagonals
+        int row =  matrix.size();
+        int col = matrix[0].size();
+
+        for(int i=0;i<row;i++){
+            for(int j=i;j<col;j++){
+                if( i == j) continue;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
-        matrix = temp;
+        int i=0;
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };

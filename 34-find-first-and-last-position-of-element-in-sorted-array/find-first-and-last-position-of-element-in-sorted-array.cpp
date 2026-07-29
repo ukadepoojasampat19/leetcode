e84441dbff723e7f57263f2ad78ee3081dc8b912
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-29 19:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (90.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
- int lowerBound(vector<int> &nums, int x){
-     int n = nums.size();
-     int l=0;
-     int h = n-1;
-     int m;
-    int ans = n;
-     while(l <= h){
-        m = (l+h)/2;
+    void first_occur(int l,int h,vector<int>& nums,int target,int& first_occ){
+        while( l<=h){
+            int m = (l+h)/2;
 
-        if(nums[m] >= x){
-            ans = m;
-            h=m-1;
-        }else{
-            l=m+1;
+            if(nums[m] == target){
+                first_occ = m;
+                h=m-1;
+            }else if(nums[m] < target){
+                l=m+1;
+            }else{
+                h = m-1;
+            }
         }
+      }
+      void last_occur(int l,int h,vector<int>& nums,int target,int& last_occ){
+        while( l<=h){
+            int m = (l+h)/2;
 
-     }
-     return ans;
-    }
-    int upperBound(vector<int> &nums, int x){
-     int n = nums.size();
-     int l=0;
-     int h = n-1;
-     int m;
-    int ans = n;
-     while(l <= h){
-        m = (l+h)/2;
-
-        if(nums[m] > x){
-            ans = m;
-            h=m-1;
-        }else{
-            l=m+1;
+            if(nums[m] == target){
+                last_occ = m;
+                l= m+1;
+            }else if(nums[m] < target){
+                l=m+1;
+            }else{
+                h = m-1;
+            }
         }
-
-     }
-     return ans;
-    }
+      }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lb = lowerBound(nums, target);
+      int n = nums.size();
+      int l=0;
+      int first_occ = -1;
+      int last_occ = -1;
+      int h=n-1;
 
-        if(lb == n || nums[lb] != target){
-            return {-1,-1};
-        }
-         
-        
-       
-       return {lb, upperBound(nums, target) -1};
+      first_occur(l,h,nums,target,first_occ);
+      last_occur(l,h,nums,target,last_occ);
+
+      return {first_occ,last_occ};
     }
 };

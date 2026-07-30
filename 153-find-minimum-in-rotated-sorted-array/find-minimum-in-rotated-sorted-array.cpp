d9058538434fe48c99e4e9acb054354813416715
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-07-30 15:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (12.16%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -9,25 +9,13 @@ public:
         while(l <= h){
             int m = (l+h)/2;
 
-            min_ele = min(min_ele, nums[m]);
-            if(nums[l] > min_ele && nums[h] > min_ele){
-                l++;
-                h--;
-                continue;
-            }
-          if(nums[l] <= min_ele  && nums[h] < min_ele){
-            int min_value = min(nums[l], nums[h]);
-
-            if(min_value ==  nums[l]){
-                h=m-1;
-            }else{
-                l = m+1;
-            }
-          }else if(nums[l] <=min_ele){
-            h = m-1;
-          }else{
-            l=m+1;
-          }
+           if(nums[l] <= nums[m]){
+             min_ele = min(min_ele, nums[l]);
+             l=m+1;
+           }else{
+            min_ele= min(min_ele, nums[m]);
+            h=m-1;
+           }
         }
         return min_ele;
     }

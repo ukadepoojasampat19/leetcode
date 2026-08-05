30c0886c38b282a73416d3c2bb2893dcd3447c5c
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2026-08-05 14:47:11 +0530

    Time: 2 ms (44.41%) | Memory: 13.9 MB (85.40%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    void recur(vector<int>& candidates, int target, int idx, int sum, vector<vector<int>>& result, vector<int>& temp) {
-        if (sum == target) {
-            result.push_back(temp);
-            return;
-        }
-        if (sum > target || idx == candidates.size()) return;
+    void getCombinationSum2(int idx,vector<int>& candidates,int target,vector<int>& res,   vector<vector<int>>& ans){
+                if(target == 0){
+                    ans.push_back(res);
+                    return;
+                }
 
-        for (int i = idx; i < candidates.size(); i++) {
-            if (i > idx && candidates[i] == candidates[i - 1]) continue; // Skip duplicates
-            temp.push_back(candidates[i]);
-            recur(candidates, target, i + 1, sum + candidates[i], result, temp);
-            temp.pop_back();
-        }
-    }
+                for(int i=idx;i<candidates.size();i++){
+                    if(i > idx && candidates[i] == candidates[i-1]) continue;
+                    if(candidates[i] > target) break;
+
+                    res.push_back(candidates[i]);
+            getCombinationSum2(i +1,candidates,target -candidates[i] ,res,ans);
+                    res.pop_back();
+                }
     
+
+    }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> temp;
-        sort(candidates.begin(), candidates.end()); // Sort to handle duplicates
-        recur(candidates, target, 0, 0, result, temp);
-        return result;
+
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> res;
+
+        getCombinationSum2(0,candidates,target,res,ans);
+        return ans;
     }
 };
